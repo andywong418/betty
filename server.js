3f653ff97d2bd83afd73aee76cbe5a7464dd0970
Andros Wong <androswong@Andross-MBP.localdomain>
2018-06-19 11:39:58 -0700

    Propagate wallet address and peer info with other hosts

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const RippleAPI = require('ripple-lib').RippleAPI
 const WebSocket = require('ws')
 if (typeof localStorage === "undefined" || localStorage === null) {
   var LocalStorage = require('node-localstorage').LocalStorage;
-  localStorage = new LocalStorage('./scratch');
+  localStorage = new LocalStorage('./scratch-' + process.env.PORT );
 }
-const wss = new WebSocket.Server({ port: 8082 })
+const wss = new WebSocket.Server({ port: Number(process.env.WEB_SOCKET) || 8002 })
 // Put logic for host key gen in here.
 
 const rippleAPI = new RippleAPI({server: 'wss://s.altnet.rippletest.net:51233'})
 // Change to wss://s1.ripple.com:443 for production
-let multiSignContract  = localStorage.getItem('sharedAccount')
+
 
 wss.on('connection', ws => {
   ws.on('message', message => {
@@ -39,15 +39,38 @@ wss.on('connection', ws => {
       localStorage.setItem('hostList', JSON.stringify({
         hostList
       }))
+      hostList.forEach(hostURL => {
+        const ws = new WebSocket(hostURL)
+        const newHostList = hostList.filter(host => {
+          return host !== hostURL
+        })
+        ws.on('open', () => {
+          ws.send(JSON.stringify({
+            "shareAddressWithPeer": true,
+            "address": address,
+            "hostList": newHostList
+          }))
+          ws.close()
+
+        })
+      })
       console.log(localStorage.getItem('sharedWalletAddress'))
       console.log(localStorage.getItem('hostList'))
       // Propagate address to every thing sent in this peerList.
+    }
+    if (message.shareAddressWithPeer) {
+      const {address, hostList} = message
+      localStorage.setItem('sharedWalletAddress', address)
+      localStorage.setItem('hostList', JSON.stringify({
+        hostList
+      }))
     }
   })
 })
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('*', (request, response, next) => {
+  const multiSignContract = localStorage.getItem('sharedWalletAddress')
   if(multiSignContract) {
     next()
   } else {
