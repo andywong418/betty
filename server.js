a14c603de436beec431805f00a130a9cfd7cdcc3
Vernon Johnson <vtj2105@columbia.edu>
2018-06-20 16:36:39 -0700

    feat: include server.js

---
+++ server.js
@@ -0,0 +1,90 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3002
+const api = require('./backend/routes')
+const RippleAPI = require('ripple-lib').RippleAPI
+const WebSocket = require('ws')
+const broker = require('./codule/n-squared')()
+const levelDB = require('./BettyDB')
+const BettyDB = new levelDB()
+
+const wss = new WebSocket.Server({ port: Number(process.env.WEB_SOCKET) || 8002 })
+// Put logic for host key gen in here.
+
+const rippleAPI = new RippleAPI({server: 'wss://s.altnet.rippletest.net:51233'})
+// Change to wss://s1.ripple.com:443 for production
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    try {
+      message = JSON.parse(message)
+    } catch (err) {
+    }
+    if (message.keyGenInitiate) {
+      rippleAPI.connect().then(() => {
+        const { address, secret } = rippleAPI.generateAddress()
+        BettyDB.set('keypair', {
+          address,
+          secret
+        })
+        ws.send(JSON.stringify({
+          address,
+          walletAddress: true
+        }))
+      })
+    }
+    if (message.sendVerifiedSharedWallet) {
+      const {address, hostList} = message
+      console.log('keygen address', address)
+      BettyDB.set('sharedWalletAddress', address)
+      BettyDB.set('hostList', {
+        hostList
+      })
+      hostList.forEach(hostURL => {
+        // Propagate address to every thing sent in this peerList.
+        const ws = new WebSocket(hostURL)
+        const newHostList = hostList.filter(host => {
+          return host !== hostURL
+        })
+        ws.on('open', () => {
+          ws.send(JSON.stringify({
+            'shareAddressWithPeer': true,
+            'address': address,
+            'hostList': newHostList
+          }))
+          ws.close()
+        })
+      })
+    }
+    if (message.shareAddressWithPeer) {
+      const {address, hostList} = message
+      console.log('peer address', address)
+      BettyDB.set('sharedWalletAddress', address)
+      BettyDB.set('hostList', {
+        hostList
+      })
+    }
+  })
+})
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use('*', async (request, response, next) => {
+  const multiSignContract = await BettyDB.get('sharedWalletAddress')
+  console.log('multiSign', multiSignContract)
+  if (multiSignContract) {
+    next()
+  } else {
+    response.send('NO XRP ACCOUNT CREATED')
+  }
+})
+app.use('/api', api)
+app.get('*', (request, response) => {
+  response.sendFile(__dirname + '/public/index.html')
+})
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(PORT, error => {
+  error
+    ? console.error(error)
+    : console.info(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
+})
